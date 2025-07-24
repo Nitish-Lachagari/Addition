47c478c0daf53889895fa13b3b1e9d49ff5de1d9
nlachagari <nlachagari@seguro-soft>
2025-07-24 11:34:24 +0530

    Added comment this is num1

--- AdditionNumber.Models/AdditionNumberModel.cs
+++ AdditionNumber.Models/AdditionNumberModel.cs
@@ -2,7 +2,7 @@
 {
     public class AdditionNumberModel
     {
-        public int Number1 {  get; set; }
+        public int Number1 {  get; set; } //this is Num1
         public int Number2 {  get; set; }
     }
 }
